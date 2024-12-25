7ce4f3cb449d594eb63477e547b11e315ea9219c
Vera Knyazeva <verashibanova@yandex.ru>
2024-12-25 14:04:14 +0500

    add service

---
+++ src/main/java/ru/javabegin/gittest/service/TestService.java
@@ -0,0 +1,14 @@
+package ru.javabegin.gittest.service;
+
+import ru.javabegin.gittest.dto.VOneResponse;
+
+public class TestService {
+
+    public VOneResponse getNameById(Long id) {
+        VOneResponse response = new VOneResponse();
+        response.setName("name");
+        response.setFirstName("firstname");
+        response.setAge(10);
+        return response;
+    }
+}
